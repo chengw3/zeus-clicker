f93d1016c3c621e309b3198a9014fa8b74ee8595
chengw3 <46546974+chengw3@users.noreply.github.com>
2025-06-14 14:43:44 +0800

    modified gamemanager explanation

--- game-manager.js
+++ game-manager.js
@@ -1,7 +1,7 @@
 import { baseBuildingStats } from "./BaseStats.js";
 import { baseUpgrades } from "./BaseStats.js";
 
-// gamemanger explains the game logic and manages the state of the game
+// modified this explanation here
 export class GameManager {
   constructor() {
     this.baseBuildingStats = JSON.parse(JSON.stringify(baseBuildingStats));
